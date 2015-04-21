32c58269a80b5329fcdee78d7b9582a47c9e189e
hui <huizhong.happy@gmail.com>
2015-04-22 03:02:18 +0800

    fantuan-5 单元测试

---
+++ src/test/com/eeeya/fantuan/BaseTest.java
@@ -0,0 +1,12 @@
+package com.eeeya.fantuan;
+
+import com.eeeya.fantuan.boot.Application;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+abstract public class BaseTest {
+
+}
