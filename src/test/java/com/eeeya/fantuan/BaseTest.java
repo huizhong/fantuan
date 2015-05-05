5fd122657aff2ba5db0ac49d95220e15aa9d0864
zhonghui <huizhong.happy@gmail.com>
2015-05-05 23:41:04 +0800

    fantuan-9 接口文档自动输出 fix test

--- src/test/java/com/eeeya/fantuan/BaseTest.java
+++ src/test/java/com/eeeya/fantuan/BaseTest.java
@@ -7,5 +7,4 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
 abstract public class BaseTest {
-
 }
--- src/test/java/com/eeeya/fantuan/config/AppConfigTest.java
+++ src/test/java/com/eeeya/fantuan/config/AppConfigTest.java
@@ -4,12 +4,14 @@ import com.eeeya.fantuan.BaseTest;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 @Component
+@WebAppConfiguration
 public class AppConfigTest extends BaseTest {
 
     @Autowired
